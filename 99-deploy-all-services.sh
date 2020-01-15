ffb968fa12848d9482a5c7be2d5a8b6a2d0904da
Carlos Vicens <cvicensa@cvicensa-mac.home>
2020-01-15 13:21:50 +0100

    All tested on RHPDS

--- 99-deploy-all-services.sh
+++ 99-deploy-all-services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./09-deploy-telegram-bot.sh
-./10-deploy-frontend-and-backend.sh
-./11-deploy-integration.sh
+./05a-run-telegram-bot.sh
+./09-deploy-frontend-and-backend.sh
+./10-deploy-integration.sh
